92dbd7fa7c876df00a37d3b2157ca9dbf10b51f3
smitrajput <smitrajputtd@gmail.com>
2025-09-05 14:25:44 -0700

    0xdkafk328ffa3

--- src/subComponents/OurSpecialities.jsx
+++ src/subComponents/OurSpecialities.jsx
@@ -25,6 +25,12 @@ const OurSpecialities = () => {
       subText: "Change your car"
     },
     {
+      id: 3,
+      title: "Electric Car",
+      icon: <MdElectricCar />,
+      subText: "Change your car"
+    },
+    {
       id: 4,
       title: "Fitness/Gym",
       icon: <MdFitnessCenter />,
