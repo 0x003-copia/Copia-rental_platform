cd5c16e7b427e1b5929ff47ec0b2872cc74cadc9
smitrajput <smitrajputtd@gmail.com>
2025-11-21 13:08:03 -0700

    smart the contract html

--- src/subComponents/Contact.jsx
+++ src/subComponents/Contact.jsx
@@ -20,17 +20,12 @@ const SubContact = () => {
             <p>Address</p>
             <span>House No.123 Sector A-1</span>
           </div>
-          <div>
-            <p>Address</p>
-            <span>House No.123 Sector A-1</span>
-          </div>
           <ul>
           <Link to={"/facebook"} target="_blank"><RiFacebookBoxLine/></Link>
           <Link to={"/facebook"} target="_blank"><RiFacebookBoxLine/></Link>
           <Link to={"/facebook"} target="_blank"><RiFacebookBoxLine/></Link>
         </ul>
         </div>
-        
         <div className="container_2">
           <h3>We'd love to hear from you</h3>
           <form>
